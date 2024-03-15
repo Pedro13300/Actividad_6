86d5b25796ff0f41b63676eed5755bb34e3391d5
Pedro José Fernández Merlo <pedrojosefm.91@gmail.com>
2024-03-15 13:08:20 +0100

    Creación de interfaz, servicio y componentes

---
+++ usuarios_Unir6/src/app/pages/usuario-view/usuario-view.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-usuario-view',
+  standalone: true,
+  imports: [],
+  templateUrl: './usuario-view.component.html',
+  styleUrl: './usuario-view.component.css'
+})
+export class UsuarioViewComponent {
+
+}
